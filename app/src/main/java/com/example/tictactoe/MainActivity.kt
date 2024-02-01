68d743d1c2b06722b847e3396f3ff0113ad4ff8f
naegerob <naeger.robert@gmail.com>
2024-02-01 23:35:33 +0100

    let tic tac toe run, but it looks bad

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.tictactoe
 
-import android.R
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
@@ -13,11 +11,10 @@ import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.tictactoe.ui.theme.Purple700
 import com.example.tictactoe.ui.theme.TicTacToeTheme
 
 class MainActivity : ComponentActivity() {
@@ -45,7 +42,7 @@ class MainActivity : ComponentActivity() {
                         val buttonModifier = Modifier
                             .size(width = 100.dp, height = 100.dp)
                             .padding(5.dp)
-                        var buttonColor = Color.Cyan
+                        var buttonColor by remember { mutableStateOf(Purple700) }
                         val symbolSize = 36.sp
 
                         val buttonTextList =
@@ -69,11 +66,11 @@ class MainActivity : ComponentActivity() {
                             Button(modifier = buttonModifier, colors = ButtonDefaults.buttonColors(backgroundColor = buttonColor),
                                     onClick = {
                                         counter = updateButton(0, buttonTextList, counter)
-                                        if("X" == checkResult(buttonTextList))
+                                        if(checkResult(buttonTextList, Player.Player1.player))
                                         {
                                             buttonColor = Color.Red
                                         }
-                                        else if("O" == checkResult(buttonTextList))
+                                        else if(checkResult(buttonTextList, Player.Player2.player))
                                         {
                                             buttonColor = Color.Green
                                         }
@@ -84,11 +81,11 @@ class MainActivity : ComponentActivity() {
                             Button(modifier = buttonModifier, colors = ButtonDefaults.buttonColors(backgroundColor = buttonColor),
                                     onClick = {
                                         counter = updateButton(1,  buttonTextList, counter)
-                                        if("X" == checkResult(buttonTextList))
+                                        if(checkResult(buttonTextList, Player.Player1.player))
                                         {
                                             buttonColor = Color.Red
                                         }
-                                        else if("O" == checkResult(buttonTextList))
+                                        else if(checkResult(buttonTextList, Player.Player2.player))
                                         {
                                             buttonColor = Color.Green
                                         }
@@ -99,11 +96,11 @@ class MainActivity : ComponentActivity() {
                             Button(modifier = buttonModifier, colors = ButtonDefaults.buttonColors(backgroundColor = buttonColor),
                                     onClick = {
                                         counter = updateButton(2, buttonTextList, counter)
-                                        if("X" == checkResult(buttonTextList))
+                                        if(checkResult(buttonTextList, Player.Player1.player))
                                         {
                                             buttonColor = Color.Red
                                         }
-                                        else if("O" == checkResult(buttonTextList))
+                                        else if(checkResult(buttonTextList, Player.Player2.player))
                                         {
                                             buttonColor = Color.Green
                                         }
@@ -117,11 +114,11 @@ class MainActivity : ComponentActivity() {
                             Button(modifier = buttonModifier, colors = ButtonDefaults.buttonColors(backgroundColor = buttonColor),
                                     onClick = {
                                         counter = updateButton(3, buttonTextList, counter)
-                                        if("X" == checkResult(buttonTextList))
+                                        if(checkResult(buttonTextList, Player.Player1.player))
                                         {
                                             buttonColor = Color.Red
                                         }
-                                        else if("O" == checkResult(buttonTextList))
+                                        else if(checkResult(buttonTextList, Player.Player2.player))
                                         {
                                             buttonColor = Color.Green
                                         }
@@ -132,11 +129,11 @@ class MainActivity : ComponentActivity() {
                             Button(modifier = buttonModifier, colors = ButtonDefaults.buttonColors(backgroundColor = buttonColor),
                                     onClick = {
                                         counter = updateButton(4, buttonTextList, counter)
-                                        if("X" == checkResult(buttonTextList))
+                                        if(checkResult(buttonTextList, Player.Player1.player))
                                         {
                                             buttonColor = Color.Red
                                         }
-                                        else if("O" == checkResult(buttonTextList))
+                                        else if(checkResult(buttonTextList, Player.Player2.player))
                                         {
                                             buttonColor = Color.Green
                                         }
@@ -147,11 +144,11 @@ class MainActivity : ComponentActivity() {
                             Button(modifier = buttonModifier, colors = ButtonDefaults.buttonColors(backgroundColor = buttonColor),
                                     onClick = {
                                         counter = updateButton(5, buttonTextList, counter)
-                                        if("X" == checkResult(buttonTextList))
+                                        if(checkResult(buttonTextList, Player.Player1.player))
                                         {
                                             buttonColor = Color.Red
                                         }
-                                        else if("O" == checkResult(buttonTextList))
+                                        else if(checkResult(buttonTextList, Player.Player2.player))
                                         {
                                             buttonColor = Color.Green
                                         }
@@ -165,11 +162,11 @@ class MainActivity : ComponentActivity() {
                             Button(modifier = buttonModifier, colors = ButtonDefaults.buttonColors(backgroundColor = buttonColor),
                                     onClick = {
                                         counter = updateButton(6, buttonTextList, counter)
-                                        if("X" == checkResult(buttonTextList))
+                                        if(checkResult(buttonTextList, Player.Player1.player))
                                         {
                                             buttonColor = Color.Red
                                         }
-                                        else if("O" == checkResult(buttonTextList))
+                                        else if(checkResult(buttonTextList, Player.Player2.player))
                                         {
                                             buttonColor = Color.Green
                                         }
@@ -180,11 +177,11 @@ class MainActivity : ComponentActivity() {
                             Button(modifier = buttonModifier, colors = ButtonDefaults.buttonColors(backgroundColor = buttonColor),
                                     onClick = {
                                         counter = updateButton(7, buttonTextList, counter)
-                                        if("X" == checkResult(buttonTextList))
+                                        if(checkResult(buttonTextList, Player.Player1.player))
                                         {
                                             buttonColor = Color.Red
                                         }
-                                        else if("O" == checkResult(buttonTextList))
+                                        else if(checkResult(buttonTextList, Player.Player2.player))
                                         {
                                             buttonColor = Color.Green
                                         }
@@ -195,11 +192,11 @@ class MainActivity : ComponentActivity() {
                             Button(modifier = buttonModifier, colors = ButtonDefaults.buttonColors(backgroundColor = buttonColor),
                                     onClick = {
                                         counter = updateButton(8, buttonTextList, counter)
-                                        if("X" == checkResult(buttonTextList))
+                                        if(checkResult(buttonTextList, Player.Player1.player))
                                         {
                                             buttonColor = Color.Red
                                         }
-                                        else if("O" == checkResult(buttonTextList))
+                                        else if(checkResult(buttonTextList, Player.Player2.player))
                                         {
                                             buttonColor = Color.Green
                                         }
@@ -209,11 +206,6 @@ class MainActivity : ComponentActivity() {
                                 Log.d(TAG, buttonTextList[8])
                             }
                         }
-
-
-
-
-
                     }
                 }
             }
@@ -243,47 +235,54 @@ class MainActivity : ComponentActivity() {
         return (counter % 2 == 0)
     }
 
-    private fun checkResult(buttonTextList: SnapshotStateList<String>) : String{
-        val checkDiagonal1 = buttonTextList[0] == buttonTextList[4]
-        val checkDiagonal2 = buttonTextList[4] == buttonTextList[8]
-        val checkDiagonal = checkDiagonal1 && checkDiagonal2
+    private fun checkResult(buttonTextList: SnapshotStateList<String>, player: String) : Boolean{
+        val checkDiagonal1 = buttonTextList[0] == player
+        val checkDiagonal2 = buttonTextList[4] == player
+        val checkDiagonal3 = buttonTextList[8] == player
+        val checkDiagonal = checkDiagonal1 && checkDiagonal2 && checkDiagonal3
         if (checkDiagonal)
         {
-            return buttonTextList[0]
+            return true
         }
 
-        val checkDiagonal3 = buttonTextList[2] == buttonTextList[4]
-        val checkDiagonal4 = buttonTextList[4] == buttonTextList[6]
-        val checkDiagonalr = checkDiagonal3 && checkDiagonal4
-        if (checkDiagonalr)
+        val checkDiagonal4 = buttonTextList[2] == player
+        val checkDiagonal5 = buttonTextList[4] == player
+        val checkDiagonal6 = buttonTextList[6] == player
+        val checkDiagonal7 = checkDiagonal4 && checkDiagonal5 && checkDiagonal6
+        if (checkDiagonal7)
         {
-            return buttonTextList[4]
+            return true
         }
 
         var offsetX = 0
         val offsetY = 3
         repeat(3) {
-            val checkColumn1 = buttonTextList[offsetX] == buttonTextList[offsetX + offsetY]
-            val checkColumn2 = buttonTextList[offsetX + offsetY] == buttonTextList[offsetX + 2 * offsetY]
-            val checkColumn = checkColumn1 || checkColumn2
+            val checkColumn1 = buttonTextList[offsetX] == player
+            val checkColumn2 = buttonTextList[offsetX + offsetY] == player
+            val checkColumn3 = buttonTextList[offsetX + 2 * offsetY] == player
+            val checkColumn = checkColumn1 && checkColumn2 && checkColumn3
             if (checkColumn)
             {
-                return buttonTextList[offsetX]
+                return true
             }
 
-            val checkRow1 = buttonTextList[offsetX * offsetY] == buttonTextList[offsetX * offsetY + 1]
-            val checkRow2 = buttonTextList[offsetX * offsetY + 1] == buttonTextList[offsetX * offsetY + 2]
-            val checkRow = checkRow1 || checkRow2
+            val checkRow1 = buttonTextList[offsetX * offsetY] == player
+            val checkRow2 = buttonTextList[offsetX * offsetY + 1] == player
+            val checkRow3 = buttonTextList[offsetX * offsetY + 2] == player
+            val checkRow = checkRow1 && checkRow2 && checkRow3
             if (checkRow)
             {
-                return buttonTextList[offsetX * offsetY]
+                return true
             }
             offsetX++
         }
-        return ""
+        return false
     }
 
-
+    enum class Player(val player: String) {
+        Player1("X"),
+        Player2("O"),
+    }
 }
 
 
