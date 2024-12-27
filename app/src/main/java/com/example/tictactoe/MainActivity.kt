f5ec72ceb00d070e64da0873736d15f4db07ac1f
naeger <naeger.robert@gmail.com>
2024-12-28 00:10:11 +0100

    works with npc and human vs. human readability is not good. refactoring is required

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
@@ -22,16 +19,23 @@ import com.example.tictactoe.ui.theme.TicTacToeTheme
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.websocket.*
+import io.ktor.serialization.kotlinx.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-        val client = HttpClient(CIO)
+
+        val client = HttpClient(CIO) {
+            install(WebSockets) {
+                contentConverter = KotlinxWebsocketSerializationConverter(Json)
+                maxFrameSize = Long.MAX_VALUE
+            }
+        }
 
         setContent {
             TicTacToeTheme {
@@ -42,8 +46,9 @@ class MainActivity : ComponentActivity() {
                     Text(text = "TicTacToe",
                         fontSize = 30.sp,
                         fontWeight = FontWeight.Bold,
-                        modifier = Modifier.wrapContentSize(Alignment.TopCenter)
-                                            .padding(16.dp))
+                        modifier = Modifier
+                            .wrapContentSize(Alignment.TopCenter)
+                            .padding(16.dp))
 
                     Column(
                         //verticalArrangement = Arrangement.spacedBy(12.dp),
@@ -51,7 +56,7 @@ class MainActivity : ComponentActivity() {
                             .wrapContentSize(Alignment.Center)
                             .padding(5.dp)
                     ) {
-                        var counter = 0
+                        var numberOfStepsPlayed = 0
                         var isGameWon by remember { mutableStateOf(false) }
                         val buttonModifier = Modifier
                             .size(width = 100.dp, height = 100.dp)
@@ -73,17 +78,30 @@ class MainActivity : ComponentActivity() {
                                     ""
                                 )
                             }
-                        Button(modifier = Modifier.height(36.dp)
-                                                    .align(CenterHorizontally),
+                        Button(modifier = Modifier
+                            .height(36.dp)
+                            .align(CenterHorizontally),
                             colors = ButtonDefaults.buttonColors(backgroundColor = buttonColor),
                             onClick = {
                                 isGameWon = false
                                 buttonColor = Purple700
                                 buttonTextList.clear()
                                 buttonTextList.addAll(List(9) { "" })
+                                numberOfStepsPlayed = 0
                         }) {
                             Text(text = "Reset Game")
                         }
+                        var gameAgainstHuman by remember { mutableStateOf(false) }
+
+                        Switch(
+                            modifier = Modifier
+                                .height(36.dp)
+                                .align(CenterHorizontally),
+                            checked = gameAgainstHuman,
+                            onCheckedChange = {
+                                gameAgainstHuman = it
+                            }
+                        )
                         Spacer(modifier = Modifier.height(36.dp))
                         for (i in 0..2) {
                             Row()
@@ -94,21 +112,36 @@ class MainActivity : ComponentActivity() {
                                         onClick = {
                                             if (isGameWon || buttonTextList[buttonId].isNotEmpty())
                                                 return@Button
-                                            counter = updateButton(buttonId, buttonTextList, counter)
-                                            if(whichPlayerClicked(counter) == Player.Player2) {
-                                                runBlocking(Dispatchers.IO) {
-                                                    buttonTextList = getNPCMove(client, buttonTextList)
-                                                }
-                                            }
-                                            if(checkResult(buttonTextList, Player.Player1.symbol))
-                                            {
+
+                                            numberOfStepsPlayed++
+                                            updateButton(buttonId, buttonTextList, numberOfStepsPlayed)
+                                            if(checkResult(buttonTextList, Player.Player1.symbol)) {
                                                 buttonColor = Color.Red
                                                 isGameWon = true
+                                                buttonTextList.clear()
+                                                buttonTextList.addAll(List(9) { "" })
+                                                return@Button
                                             }
-                                            else if(checkResult(buttonTextList, Player.Player2.symbol))
-                                            {
+                                            else if(gameAgainstHuman and checkResult(buttonTextList, Player.Player2.symbol)) {
                                                 buttonColor = Color.Green
+                                                buttonTextList.clear()
+                                                buttonTextList.addAll(List(9) { "" })
                                                 isGameWon = true
+                                                return@Button
+                                            }
+
+                                            if(!gameAgainstHuman and (whichPlayerClicked(numberOfStepsPlayed) == Player.Player1)) {
+                                                numberOfStepsPlayed++
+                                                runBlocking(Dispatchers.IO) { // equivalent as updateButton for human
+                                                    buttonTextList = getNPCMove(client, buttonTextList)
+                                                }
+                                                if(checkResult(buttonTextList, Player.Player2.symbol)) {
+                                                    buttonColor = Color.Green
+                                                    isGameWon = true
+                                                    buttonTextList.clear()
+                                                    buttonTextList.addAll(List(9) { "" })
+                                                    return@Button
+                                                }
                                             }
                                         }) {
                                         Text(buttonTextList[buttonId], fontSize = symbolSize)
@@ -122,21 +155,17 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    private fun updateButton(button: Int, buttonTextList : SnapshotStateList<String>, counter: Int) : Int {
-        var counterPost = counter
-        if(button in 0..8)
-        {
-            counterPost++
-            buttonTextList[button] = if (whichPlayerClicked(counterPost) == Player.Player2) Player.Player2.symbol else Player.Player1.symbol
+    private fun updateButton(button: Int, buttonTextList : MutableList<String>, counter: Int) {
+        if(button in 0..8) {
+            buttonTextList[button] = if (whichPlayerClicked(counter) == Player.Player2) Player.Player2.symbol else Player.Player1.symbol
         }
-        return counterPost
     }
 
     private fun whichPlayerClicked(counter: Int): Player {
         return if(counter % 2 == 0) Player.Player2 else Player.Player1
     }
 
-    private fun checkResult(buttonTextList: SnapshotStateList<String>, player: String): Boolean {
+    private fun checkResult(buttonTextList: MutableList<String>, player: String): Boolean {
         val winPatterns = listOf(
             listOf(0, 1, 2), // First row
             listOf(3, 4, 5), // Second row
@@ -147,6 +176,10 @@ class MainActivity : ComponentActivity() {
             listOf(0, 4, 8), // Diagonal from top-left to bottom-right
             listOf(2, 4, 6)  // Diagonal from top-right to bottom-left
         )
+
+        if(buttonTextList.isEmpty()) {
+            throw Exception("buttonTestList is empty")
+        }
         for (pattern in winPatterns) {
             if (pattern.all { buttonTextList[it] == player }) {
                 return true
@@ -160,19 +193,23 @@ class MainActivity : ComponentActivity() {
         Player2("X"),
     }
 
-    suspend fun getNPCMove(client: HttpClient, gameField: MutableList<String>): GameField? {
-        var gamefieldSerReturn: GameField? = null
-        client.webSocket(
-            host = "localhost",
-            port = 8080,
-            path = "/tictactoe"
-        ) {
-            val gameFieldSer = GameField(gameField)
-
-            sendSerialized(gameFieldSer)
-            gamefieldSerReturn = receiveDeserialized<GameField>()
+    suspend fun getNPCMove(client: HttpClient, gameField: SnapshotStateList<String>): SnapshotStateList<String> {
+        val buttonTextListResponse: SnapshotStateList<String> = mutableStateListOf<String>()
+        try {
+            client.webSocket(
+                host = "192.168.1.4",
+                port = 8080,
+                path = "/tictactoe"
+            ) {
+                val gameFieldSer = GameField(gameField.toMutableList())
+                sendSerialized(gameFieldSer)
+                val gamefieldSerReturn = receiveDeserialized<GameField>()
+                buttonTextListResponse.addAll(gamefieldSerReturn.gameField)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
-        return gamefieldSerReturn
+        return buttonTextListResponse
     }
 
     @Serializable
